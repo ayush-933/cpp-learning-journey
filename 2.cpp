cc194a25438099ae25fbff99b7cfbdd5775d0671
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-03 19:24:37 +0530

    variables

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name="ayush";
+    int age=20;
+    cout<<"my name is "<<name<<endl;
+    cout<<"i am "<<age<<" year old this year"<<endl;
+    name="john";
+    age=19;
+    cout<<"my name is "<<name<<endl;
+    cout<<"i am "<<age<<" year old this year"<<endl;
+    return 0;
+}
