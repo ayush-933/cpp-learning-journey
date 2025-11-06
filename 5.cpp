706f226f01e74faf963998a4e3facc3e3974bd93
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-06 23:12:23 +0530

    math operations

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+cout<<pow(2,5)<<"\n";
+cout<<sqrt(64)<<"\n";
+cout<<round(4.6)<<"\n";
+cout<<ceil(4.3)<<"\n";
+cout<<floor(4.8)<<"\n";
+cout<<fmax(4,5.6)<<"\n";
+cout<<fmin(4,5.6)<<"\n";
+return 0;}
