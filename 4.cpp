c44c5826c03d132eb2e13f7517dc07660bf57308
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-06 23:03:58 +0530

    arithematic

---
+++ 4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<41+5<<"\n";
+cout<<41-5<<"\n";
+cout<<41*5<<"\n";
+cout<<41/5<<"\n";
+cout<<41%5<<"\n";
+float x=41.0/5.0;
+cout<<x;
+return 0;}
