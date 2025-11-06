f27b380ba3a5d0fd61efea504618c71a93732ccf
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-06 22:56:34 +0530

    string

---
+++ 3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+string phrase="ayush srivastava";
+cout<<phrase[7]<<"\n";
+cout<<phrase.substr(0,3)<<"\n";
+cout<<phrase.length();
+return 0;}
