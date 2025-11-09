3b89cd4373e9fca0132adc00939b049e20b4ec02
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-09 10:39:31 +0530

    array

---
+++ 9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[5];
+for(int i=0;i<5;i++){
+cout<<"enter the "<<i<<"th element of the array: \n ";
+cin>>arr[i];
+
+}
+for(int j=0;j<5;j++){
+
+cout<<arr[j]<<"\n";
+}
+
+return 0;}
