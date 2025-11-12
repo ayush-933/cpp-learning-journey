5781163364a3b91ff724bb886b8ba83f298b55e2
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-12 10:09:27 +0530

    functions

---
+++ 10.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void intro(string name,int age){
+    cout<<"your name is "<<name<< " and your age is "<< age;
+}
+int main(){
+intro("ayush",20);
+    
+return 0;
+}
