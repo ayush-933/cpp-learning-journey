cb94cd3157e99aaca6c8926bf3904e4d632a6d03
Ayush Srivastava <srivastavayush040@gmail.com>
2025-11-22 10:31:05 +0530

    switch

--- 13.cpp
+++ 13.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 
 
+
+
 int main(){
-   
+   int day;
+cout<<"enter the number:";
+cin>>day;
+switch(day){
+    case 0:
+    cout<<"sunday";
+    break;
+    case 1:
+    cout<<"monday";
+    break;
+    case 2:
+    cout<<"tuesday";
+    break;
+    case 3:
+    cout<<"wednesday";
+    break;
+    case 4:
+    cout<<"thursday";
+    break;
+    case 5:
+    cout<<"friday";
+    break;
+    case 6:
+    cout<<"saturday";
+    break;
+}
 return 0;
 }
