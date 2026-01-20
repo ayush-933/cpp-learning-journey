ca6890c49ce3953832bb2c77320fcd84cfbede04
Ayush Srivastava <srivastavayush040@gmail.com>
2026-01-20 21:35:49 +0530

    dsa;pattern

---
+++ 23.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int count=0;
+
+
+        for(int i=0;i<3;i++){
+               for(int j=1;j<4;j++){
+                  count++;
+                    cout<<count<<" ";
+               }
+                cout<<endl;
+        }
+return 0;}
