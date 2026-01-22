753298f15b9bb77391d08319547f7c81b8c109b9
Ayush Srivastava <srivastavayush040@gmail.com>
2026-01-22 22:50:22 +0530

    dsa:pattern

---
+++ 27.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+          int n;
+          cin>>n;
+               for(int i=1;i<=n;i++){
+                        for(int j=n-i;j>0;j--){
+                            cout<<" ";
+
+                        }
+                       for(int k=1;k<=i;k++){
+                            cout<<k;
+                            
+                       } 
+                       for(int m=i-1;m>0;m--){
+                          
+                           
+                            cout<<m;
+                       }
+                          
+                          cout<<endl;
+               }
+
+return 0;}
