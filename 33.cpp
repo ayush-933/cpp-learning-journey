0b17105fe0d213c728197d962603689b46b424f3
Ayush Srivastava <srivastavayush040@gmail.com>
2026-03-06 22:11:12 +0530

    search

--- 33.cpp
+++ 33.cpp
@@ -12,7 +12,7 @@ int main(){
    int key;
    cout<<"enter the element you want to search:"<<endl;
    cin>>key;
-   int s=0,e=n;
+   int s=0,e=n-1;
    while(1){
     int mid=(s+e)/2;
     if(a[mid]>key){
